5d2393f0ac35ad067722dd18d09b21dbf1c243c8
hmbrandt <mario.hildebrandt@ymail.com>
2014-11-18 11:27:56 +0100

    remove old stuff

--- CareDroid/gen/de/tud/wise/caredroid/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package de.tud.wise.caredroid;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- CareDroid/src/de/tud/wise/caredroid/model/MetaObject.java
+++
@@ -1,5 +0,0 @@
-package de.tud.wise.caredroid.model;
-
-public abstract class MetaObject implements Visitable{
-	
-}
