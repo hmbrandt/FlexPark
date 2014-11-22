d0221307d24adceb3c8f227a65a7d014d3b4d485
Cimpso <Cimpso@gmx.de>
2014-11-22 13:55:47 +0100

    login and regsiter

--- FlexPark/src/com/admuc/flexpark/MainActivity.java
+++ FlexPark/src/com/admuc/flexpark/MainActivity.java
@@ -2,6 +2,7 @@ package com.admuc.flexpark;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -13,21 +14,27 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+    }
+    
+    public void openMap2 (View view) {
+        
         setContentView(R.layout.main);
         
         GoogleMap googleMap;
         googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         
         googleMap.setMyLocationEnabled(true);
-//        googleMap.getUiSettings().setZoomGesturesEnabled(true);
+
     }
+    
+    protected void openRegister (View view) {
+        setContentView(R.layout.register);
+        
+    }
+    
 	
-//	@Override
-//	public void onCreate(Bundle savedInstanceState) {
-//	    super.onCreate(savedInstanceState);
-//	    setContentView(R.layout.main);
-//	    MapView mapView = (MapView) findViewById(R.id.map);
-//	    mapView.setBuiltInZoomControls(true);
-//	}
+
 
 }
